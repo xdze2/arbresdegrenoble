f15a52cc0539ee73039c6b50ef7bb7b97bfb7819
etienne <xdze2.me@gmail.com>
2017-12-06 23:28:26 +0100

    typo

--- sql_querries.sql
+++ sql_querries.sql
@@ -1,5 +1,5 @@
 
--- Liste les (genre, espece, variete)
+-- Liste les tuples (genre, espece, variete)
 
 SELECT count(*) AS c, genre_bota, espece, variete FROM arbres
 GROUP BY genre_bota, espece, variete
